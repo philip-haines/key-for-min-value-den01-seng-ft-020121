6185df560f7b54bcbff38af0b97e10dc0d2ac139
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-29 23:58:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest_value = 100000
-  key = key
+  smallest_value = 0
+  key = nil
   name_hash.each do |name, value|
     if value < smallest_value
       smallest_value = value
